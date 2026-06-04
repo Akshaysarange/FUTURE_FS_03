1ebbf5a53f57e3b10322fc8812548f48f4a72acd
Akshay Sarange <akshaysarange8@gmail.com>
2026-06-04 19:54:28 +0530

    menu js

---
+++ menu.js
@@ -0,0 +1,19 @@
+function filterMenu(category){
+
+let items = document.querySelectorAll(".menu-card");
+
+items.forEach(item => {
+
+if(category === "all"){
+item.style.display = "block";
+}
+else if(item.classList.contains(category)){
+item.style.display = "block";
+}
+else{
+item.style.display = "none";
+}
+
+});
+
+}
